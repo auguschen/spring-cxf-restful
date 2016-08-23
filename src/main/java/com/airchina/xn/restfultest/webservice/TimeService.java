71e6776552346a80bcb0355cd464ba43da464b1d
auguschen <tianhao.chen@gmail.com>
2016-08-23 15:22:51 +0800

    create runable app

---
+++ src/main/java/com/airchina/xn/restfultest/webservice/TimeService.java
@@ -0,0 +1,6 @@
+package com.airchina.xn.restfultest.webservice;
+
+public interface TimeService {
+
+	public String getDateTime();
+}
---
+++ src/main/java/com/airchina/xn/restfultest/webservice/impl/TimeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.airchina.xn.restfultest.webservice.impl;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.stereotype.Service;
+
+import com.airchina.xn.restfultest.webservice.TimeService;
+
+@Service("timeService")
+@Path("time")
+public class TimeServiceImpl implements TimeService {
+
+	@Override
+	@GET
+	@Produces("text/plain")
+	public String getDateTime() {
+		DateFormatter formatter = new DateFormatter("dd/MM/yyyy hh:mm:ss"); 
+        return formatter.print(Calendar.getInstance().getTime(), Locale.getDefault()); 
+//		return "Get Time Here. this is jus a demo.";
+	}
+
+}
